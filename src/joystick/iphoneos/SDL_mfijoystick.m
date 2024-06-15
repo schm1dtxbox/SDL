31f7acaa1b291d3f88149f14290b3195f630905d
schm1dtxbox <36817325+schm1dtxbox@users.noreply.github.com>
2024-06-15 13:04:07 +0100

    Update SDL_mfijoystick.m

--- src/joystick/iphoneos/SDL_mfijoystick.m
+++ src/joystick/iphoneos/SDL_mfijoystick.m
@@ -1737,10 +1737,12 @@ static int IOS_JoystickSendEffect(SDL_Joystick *joystick, const void *data, int 
             GCDualSenseAdaptiveTrigger *adaptiveTrigger = dualSense.rightTrigger;
             float resistiveStrength = SDL_min(1, 0.4 + adaptiveTrigger.value);
             if ( adaptiveTrigger.value < 0.9 ) {
+              [GCDualSenseAdaptiveTrigger setModeFeedbackWithStartPosition];
               [adaptiveTrigger setModeFeedbackWithStartPosition:
                 0,
                 resistiveStrength];
             } else {
+              [GCDualSenseAdaptiveTrigger setModeVibrationWithStartPosition];
               [adaptiveTrigger setModeVibrationWithStartPosition:
                 0,
                 resistiveStrength,
