e4017ea8b8dcfc7edda5cfee189805e961983fca
schm1dtxbox <36817325+schm1dtxbox@users.noreply.github.com>
2024-06-15 14:18:23 +0100

    Update SDL_mfijoystick.m

--- src/joystick/iphoneos/SDL_mfijoystick.m
+++ src/joystick/iphoneos/SDL_mfijoystick.m
@@ -1785,9 +1785,9 @@ static int IOS_JoystickSendEffect(SDL_Joystick *joystick, const void *data, int 
                 endPosition: 0.4
                 resistiveStrength: 0.5];
             } else if ( state->rgucRightTriggerEffect[0] == 0x06 ) {
-              [rightadaptiveTrigger setModeVibrationWithStartPosition: 0.4
+              [rightadaptiveTrigger setModeVibrationWithStartPosition: state->rgucRightTriggerEffect[3] / 255.f
                 amplitude: 1.0
-                frequency: 0.05];
+                frequency: state->rgucRightTriggerEffect[1] / 255.f];
             } else {
               [rightadaptiveTrigger setModeOff];
             }
