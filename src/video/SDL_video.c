1376f169d2462dad64739124b52df388c64961b0
schm1dtxbox <36817325+schm1dtxbox@users.noreply.github.com>
2024-06-29 14:03:50 +0100

    Update SDL_video.c

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -1628,8 +1628,10 @@ SDL_Window *SDL_CreateWindow(const char *title, int x, int y, int w, int h, Uint
     Uint32 type_flags, graphics_flags;
     if( x == SDL_WINDOWPOS_CENTERED )
         x = 0;
+        w = 1280;
     if( y == SDL_WINDOWPOS_CENTERED )
         y = 0;
+        h = 800;
 
     if (!_this) {
         /* Initialize the video system if needed */
