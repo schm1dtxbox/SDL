e9834c800d92c9b5c9ebd3859d29febdc169abf7
schm1dtxbox <36817325+schm1dtxbox@users.noreply.github.com>
2024-06-29 13:47:23 +0100

    Update SDL_cocoawindow.m

--- src/video/cocoa/SDL_cocoawindow.m
+++ src/video/cocoa/SDL_cocoawindow.m
@@ -1793,7 +1793,7 @@ void Cocoa_SetWindowSize(_THIS, SDL_Window * window)
     [nswindow setFrame:[nswindow frameRectForContentRect:rect] display:YES];
     s_moveHack = moveHack;
 
-    ScheduleContextUpdates(windata);
+    /*ScheduleContextUpdates(windata);*/
 }}
 
 void Cocoa_SetWindowMinimumSize(_THIS, SDL_Window * window)
@@ -2045,7 +2045,7 @@ void Cocoa_SetWindowFullscreen(_THIS, SDL_Window * window, SDL_VideoDisplay * di
         [data.listener resumeVisibleObservation];
     }
 
-    /*ScheduleContextUpdates(data);*/
+    ScheduleContextUpdates(data);
 }}
 
 int Cocoa_SetWindowGammaRamp(_THIS, SDL_Window * window, const Uint16 * ramp)
